f3ed076dbcebecdcca51388bbcdc3f5fcc077e86
Kevin O'Leary <kevin@the-citizenry.com>
2024-06-09 01:24:45 -0500

    feat: add terser webpack plugin

--- packages/slate-tools/tools/webpack/config/prod.js
+++ packages/slate-tools/tools/webpack/config/prod.js
@@ -3,9 +3,10 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const SlateConfig = require('@shopify/slate-config');
 const SlateTagPlugin = require('@shopify/slate-tag-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const babel = require('./parts/babel');
 const sass = require('./parts/sass');
@@ -39,9 +40,11 @@ module.exports = merge([
         'process.env': {NODE_ENV: '"production"'},
       }),
 
-      new UglifyJSPlugin({
-        sourceMap: true,
-      }),
+      new TerserPlugin(),
+
+      // new UglifyJSPlugin({
+      //   sourceMap: true,
+      // }),
 
       // generate dist/layout/*.liquid for all layout files with correct paths to assets
 
